1edbb55f344637742070edd6ca505734bf5dfd25
SilvenLEAF <SilvenLEAF@gmail.com>
2020-10-04 16:40:10 +0530

    Updates

---
+++ precache-manifest.6417fb4cf27975a90f706e4c8befd0a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8589fde123bbe5da3b3f9d23d3696c2d",
+    "url": "/Soul/index.html"
+  },
+  {
+    "revision": "28e012d2b538bb89a20a",
+    "url": "/Soul/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "f096a24b61efd3f7c673",
+    "url": "/Soul/static/css/main.6eeb1460.chunk.css"
+  },
+  {
+    "revision": "28e012d2b538bb89a20a",
+    "url": "/Soul/static/js/2.fb372ae4.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/Soul/static/js/2.fb372ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f096a24b61efd3f7c673",
+    "url": "/Soul/static/js/main.bf48af27.chunk.js"
+  },
+  {
+    "revision": "6d84ac043e472b7d511c",
+    "url": "/Soul/static/js/runtime-main.bf333518.js"
+  },
+  {
+    "revision": "d6685ae3843eadf33d86ba4ebd0d149f",
+    "url": "/Soul/static/media/landingIcon.d6685ae3.jpg"
+  }
+]);
